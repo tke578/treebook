2c6975e53f027900bedb7d79622c16374b6d5664
Andrew Garcia <tke578@gmail.com>
2013-12-13 15:38:58 -0800

    Application Configuration

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ if defined?(Bundler)
  #Bundler.require(:default, :assets, Rails.env)
 end
 
-config.assets.initialize_on_precompile = true
+config.assets.initialize_on_precompile = false
 
 module Treebook
   class Application < Rails::Application
