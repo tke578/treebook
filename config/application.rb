7b4599d8d233982bcebba6a8014ee7361356b1f7
Andrew Garcia <tke578@gmail.com>
2013-12-14 09:32:01 -0800

    Configure application

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ if defined?(Bundler)
  #Bundler.require(:default, :assets, Rails.env)
 end
 
-config.assets.initialize_on_precompile = false
+
 
 module Treebook
   class Application < Rails::Application
@@ -60,6 +60,8 @@ module Treebook
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.initialize_on_precompile = false
     
   end
 end
