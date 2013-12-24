6fc0bbc22a82631a0a693fcb5584a5a43ebe42fb
Andrew Garcia <tke578@gmail.com>
2013-12-23 20:08:29 -0800

    Email configure

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "gmail",
+  :domain               => "gmail.com",
   :user_name            => "treehouse8973@gmail.com",
   :password             => "3235691914",
   :authentication       => "plain",
