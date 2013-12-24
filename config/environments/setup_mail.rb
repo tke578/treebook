bd8bf7dddf8baef2ba5b811572b533150fc47024
Andrew Garcia <tke578@gmail.com>
2013-12-23 20:20:52 -0800

    Email configure

---
+++ config/environments/setup_mail.rb
@@ -0,0 +1,10 @@
+config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "gmail.com",
+  :user_name            => "treehouse8973@gmail.com",
+  :password             => "3235691914",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
